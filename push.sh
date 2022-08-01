4f64a8d7ce97b0822f52fd250b6fe6144ee11152
David Kopp <git@davidkopp.de>
2022-08-01 13:41:59 +0200

    Fix date format

--- push.sh
+++ push.sh
@@ -1,9 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 source $HOME/repo.conf
+printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1 
+
 cd $HOME/storage/shared/$GH_REPO
 git add .
-git commit -m "android on $(date)"
+git commit -m "android on $(date +%Y-%m-%d" "%H:%M:%S)"
 git push
 cd $HOME
 bash -c "read -t 3 -n 1"

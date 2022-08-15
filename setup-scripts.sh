0502cac6067ed357c1469fbc18957d200df2ef53
David Kopp <git@davidkopp.de>
2022-08-15 22:18:20 +0200

    Add git sync script

--- setup-scripts.sh
+++ setup-scripts.sh
@@ -7,7 +7,7 @@ mkdir -p $HOME/.shortcuts
 chmod 700 -R $HOME/.shortcuts
 
 # Copy scripts
-rsync -a --include='log.sh' --include='pull.sh' --include='push.sh' --exclude '*' $HOME/termux-scripts/ $HOME/.shortcuts
+rsync -a --include='log.sh' --include='pull.sh' --include='push.sh' --include='sync.sh' --exclude '*' $HOME/termux-scripts/ $HOME/.shortcuts
 
 chmod +x $HOME/.shortcuts/*.sh
 
@@ -18,6 +18,6 @@ mkdir -p $HOME/.termux/tasker
 chmod 700 -R $HOME/.termux
 
 # Copy scripts
-rsync -a --include='log.sh' --include='pull.sh' --include='push.sh' --exclude '*' $HOME/termux-scripts/ $HOME/.termux/tasker
+rsync -a --include='log.sh' --include='pull.sh' --include='push.sh' --include='sync.sh' --exclude '*' $HOME/termux-scripts/ $HOME/.termux/tasker
 
 chmod +x $HOME/.termux/tasker/*.sh
---
+++ sync.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+source $HOME/repo.conf
+cd $HOME/storage/shared/$GH_REPO
+git pull
+
+printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1 
+git add .
+git commit -m "android on $(date +%Y-%m-%d" "%H:%M:%S)"
+git push
+
+cd $HOME
+bash -c "read -t 3 -n 1"

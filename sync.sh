cd5c0738d08e53dfc42069928c559748b6062287
David Kopp <git@davidkopp.de>
2022-08-16 14:38:45 +0200

    Fix sync script

--- sync.sh
+++ sync.sh
@@ -2,12 +2,13 @@
 
 source $HOME/repo.conf
 cd $HOME/storage/shared/$GH_REPO
-git pull
 
 printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1 
 git add .
 git commit -m "android on $(date +%Y-%m-%d" "%H:%M:%S)"
 git push
+
+git pull
 
 cd $HOME
 bash -c "read -t 3 -n 1"

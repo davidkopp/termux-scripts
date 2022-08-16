24fd5db6346d75fe3515a4d30ede3384f951ca06
David Kopp <git@davidkopp.de>
2022-08-16 15:49:22 +0200

    Fix newline

--- sync.sh
+++ sync.sh
@@ -3,20 +3,20 @@
 source $HOME/repo.conf
 cd $HOME/storage/shared/$GH_REPO
 
-echo "Syncing ${PWD##*/}\n"
-echo "1. Pull\n------\n"
+echo -e "Syncing ${PWD##*/}\n"
+echo -e "1. Pull\n------\n"
 git pull
 
-echo "2. Commit new changes and push\n------\n"
+echo -e "2. Commit new changes and push\n------\n"
 printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1 
 git add .
 git commit -m "android on $(date +%Y-%m-%d" "%H:%M:%S)"
 git push
 
-echo "3. Pull\n------\n"
+echo -e "3. Pull\n------\n"
 git pull
 
-echo "4. All done\n"
+echo -e "4. All done\n"
 
 # Wait for Enter 
 bash -c "read -t 3 -n 1"

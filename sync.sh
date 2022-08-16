a0d3c97a7f49577d32903613816393d7812e6553
David Kopp <git@davidkopp.de>
2022-08-16 15:53:04 +0200

    Improve echos

--- sync.sh
+++ sync.sh
@@ -3,20 +3,20 @@
 source $HOME/repo.conf
 cd $HOME/storage/shared/$GH_REPO
 
-echo -e "Syncing ${PWD##*/}\n"
-echo -e "1. Pull\n------\n"
+echo -e "Syncing repo '${PWD##*/}'...\n"
+echo -e "1. Pull\n"
 git pull
 
-echo -e "2. Commit new changes and push\n------\n"
+echo -e "\n2. Commit new changes and push\n"
 printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1 
 git add .
 git commit -m "android on $(date +%Y-%m-%d" "%H:%M:%S)"
 git push
 
-echo -e "3. Pull\n------\n"
+echo -e "\n3. Pull\n"
 git pull
 
-echo -e "4. All done\n"
+echo -e "\n4. All done\n"
 
 # Wait for Enter 
 bash -c "read -t 3 -n 1"

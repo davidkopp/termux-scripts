8df94025726bbcd24c05b3e0bfd3697711c01f31
David Kopp <git@davidkopp.de>
2023-12-27 16:16:32 +0100

    Fix go-to-repo

--- go-to-repo.sh
+++ go-to-repo.sh
@@ -1,11 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Get repo name either by argument or use env var 'GH_REPO'
-if [ -e "$HOME/repo.conf" ]; then
-  source $HOME/repo.conf
-elif [ -n "$1" ]; then
+# Get repo name either by argument or by reading file 'repo.conf'
+if [ -n "$1" ]; then
   GH_REPO=$1
-elif [ -z "${GH_REPO}" ]; then
+elif [ -e "$HOME/repo.conf" ]; then
+  source $HOME/repo.conf
+else
   echo "No argument provided and file 'repo.conf' in your home directory does not exist!"
   exit 1
 fi

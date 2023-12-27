24a3806fd4bd6dd35ddd3b36436a68c700e6fddf
David Kopp <git@davidkopp.de>
2023-12-27 15:50:17 +0100

    Fix go-to-repo

--- go-to-repo.sh
+++ go-to-repo.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Get repo name either by argument or use env var 'GH_REPO'
-if [ -z "$1" ]; then
+if [ -n "$1" ]; then
   GH_REPO=$1
-elif [ -z "${GH_REPO+x}" ]; then
+elif [ -z "${GH_REPO}" ]; then
   echo "No argument provided and environment variable 'GH_REPO' is not set!"
   exit 1
 fi
